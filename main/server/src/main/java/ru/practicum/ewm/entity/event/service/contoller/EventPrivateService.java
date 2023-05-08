87ed2997b5a9246015955b30d6c888d97213a3f1
Klementor <mihailkrylov8189@gmail.com>
2023-05-08 12:07:16 +0300

    fixes

--- main/server/src/main/java/ru/practicum/ewm/entity/event/service/contoller/EventPrivateService.java
+++ main/server/src/main/java/ru/practicum/ewm/entity/event/service/contoller/EventPrivateService.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.entity.event.service.contoller;
 
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.entity.event.dto.request.AddEventRequestDto;
 import ru.practicum.ewm.entity.event.dto.request.UpdateEventUserRequestDto;
 import ru.practicum.ewm.entity.event.dto.request.comment.AddCommentRequestDto;
@@ -16,6 +17,7 @@ public interface EventPrivateService {
 
     EventFullResponseDto getEventById(Long userId, Long eventId);
 
+    @Transactional
     CommentResponseDto addComment(Long userId, Long eventId, AddCommentRequestDto commentDto);
 
     Iterable<EventShortResponseDto> getUserEvents(Long userId, Integer from, Integer size);
@@ -31,6 +33,7 @@ public interface EventPrivateService {
             Long eventId,
             UpdateEventUserRequestDto eventDto);
 
+    @Transactional
     CommentResponseDto updateCommentById(
             Long userId,
             Long eventId,
@@ -43,5 +46,6 @@ public interface EventPrivateService {
             Long eventId,
             UpdateEventParticipationStatusRequestDto requestStatusDto);
 
+    @Transactional
     void deleteCommentById(Long userId, Long eventId, Long comId);
 }
